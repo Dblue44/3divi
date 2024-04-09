f9abf726c99e9686cafdc83693e1034ab08fb7de
Dmitrii <stakheev12@mail.ru>
2024-04-09 21:21:18 +0500

    Replace to threading

--- client/tasks/routes.py
+++ client/tasks/routes.py
@@ -1,11 +1,13 @@
-import asyncio
 import random
+import threading
+from threading import Thread
 from typing import List
+import asyncio
+import requests
+from requests import Session
 
-import aiohttp
-
-from client.config import settings
 from client.logger import logger
+from client.config import settings
 from pydantic import BaseModel, Field
 from fastapi import (
     status,
@@ -27,19 +29,34 @@ class TasksData(BaseModel):
     delay_range: float = Field(ge=0)
 
 
-async def sendTask(task: Task) -> None:
-    async with aiohttp.ClientSession() as session:
-        request = await session.post(url=settings.RECEIVER_URL, json=task.model_dump())
-        logger.info(f"Send task {task.model_dump_json()}. Thread: {asyncio.current_task()}. Request: {request}")
+class TaskResponse(BaseModel):
+    asyncAnswer: str = Field(default="ok")
+
+
+async def sendTask(task: Task, session: Session) -> None:
+    request = requests.Request('POST', settings.RECEIVER_URL, json=task.model_dump())
+    prepared_request = request.prepare()
+    session.send(prepared_request)
+    logger.info(f"Send task {task.model_dump_json()}. "
+                f"Thread: {threading.current_thread().name} "
+                f"Request: {prepared_request.method} url: {prepared_request.url} - "
+                f"headers: {prepared_request.headers} body: {prepared_request.body}")
+
+
+def startTasks(value: int, delayRange: int) -> None:
+    session = requests.Session()
+    for i in range(value):
+        asyncio.run(sendTask(Task(id=i, delay=random.randint(0, delayRange)), session))
 
 
 @router.post("/setTask")
 async def setTask(tasks: TasksData) -> Response:
+    threads: List[Thread] = []
+    taskPerThread = tasks.connection_value // tasks.connection_count
     for i in range(tasks.connection_count):
-        task = asyncio.create_task(
-            sendTask(
-                Task(id=i, delay=random.randint(0, tasks.delay_range))
-            )
-        )
-        await task
-    return Response(status=status.HTTP_201_CREATED, content={"asyncAnswer": "ok"})
+        thread = Thread(target=startTasks, args=(taskPerThread, tasks.delay_range,))
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
+    return Response(status_code=status.HTTP_201_CREATED, content=TaskResponse().model_dump_json())
